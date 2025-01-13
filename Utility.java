bef7568dbb3b6fa496c3c2de46cb303cbf088c27
Hamza-Alhalabi-03 <hamza.alhalabi03@gmail.com>
2025-01-13 21:45:18 +0300

    Michael added a method to the utility class

--- Utility.java
+++ Utility.java
@@ -4,8 +4,17 @@ public class Utility {
         return number % 2 == 0;
     }
 
+    public static String reverseString(String str) {
+        if (str == null) {
+            return null;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+
     public static void main(String[] args) {
         // Test isEven method
         System.out.println("Is 10 even? " + isEven(10));
+
+        System.out.println("Reverse of 'hello': " + reverseString("hello"));
     }
 }
