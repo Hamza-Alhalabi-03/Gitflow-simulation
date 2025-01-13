db0d0caf0f42dcd6411471cf328e478a7fac2044
Hamza-Alhalabi-03 <hamza.alhalabi03@gmail.com>
2025-01-14 00:34:32 +0300

    Michael added a method utility class

--- Utility.java
+++ Utility.java
@@ -15,6 +15,17 @@ public class Utility {
         return number * number;
     }
 
+    public static int factorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot accept negative numbers");
+        }
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Is 10 even? " + isEven(10));
